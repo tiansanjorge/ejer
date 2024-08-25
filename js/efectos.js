ccaf27d5fab67eb4d3dd86370428345f7dbdf540
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2024-08-25 20:58:00 -0300

    form error fix | EmailJS

--- js/efectos.js
+++ js/efectos.js
@@ -1,135 +1,159 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  var inicio = $("#inicio").offset().top,
+    quienes = $("#quienes").offset().top,
+    eligen = $("#eligen").offset().top,
+    contacto = $("#contacto").offset().top;
 
-	var inicio = $('#inicio').offset().top,
-		quienes = $('#quienes').offset().top,
-		eligen = $('#eligen').offset().top,
-		contacto = $('#contacto').offset().top;
+  $("#btn-inicio").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: inicio - 100,
+      },
+      500
+    );
+  });
 
-	$('#btn-inicio').on('click',function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: inicio -100
-		}, 500);
-	});
+  $("#btn-quienes").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: quienes - 100,
+      },
+      500
+    );
+  });
 
-	$('#btn-quienes').on('click',function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: quienes -100
-		}, 500);
-	});
-	
-	$('#btn-eligen').on('click',function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: eligen -100
-		}, 500);
-	});
+  $("#btn-eligen").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: eligen - 100,
+      },
+      500
+    );
+  });
 
-	$('#btn-contacto').on('click',function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: contacto -100
-		}, 500);
-	});
+  $("#btn-contacto").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: contacto - 100,
+      },
+      500
+    );
+  });
 
-	$('#btn-contacto2').on('click',function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: inicio -100
-		}, 500);
-	});
+  $("#btn-contacto2").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: inicio - 100,
+      },
+      500
+    );
+  });
 
-	$('#btn-iniciox').on('click',function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: inicio
-		}, 500);
-	});
+  $("#btn-iniciox").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: inicio,
+      },
+      500
+    );
+  });
 
-	$('#btn-quienesx').on('click',function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: quienes
-		}, 500);
-	});
-	
-	$('#btn-eligenx').on('click',function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: eligen
-		}, 500);
-	});
+  $("#btn-quienesx").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: quienes,
+      },
+      500
+    );
+  });
 
-	$('#btn-contactox').on('click',function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: contacto
-		}, 500);
-	});
+  $("#btn-eligenx").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: eligen,
+      },
+      500
+    );
+  });
 
-	$("#sendMailForm").submit(function(e){
-		e.preventDefault();
-		Swal.fire({
-		  icon: 'success',
-		  title: 'Éxito',
-		  text: 'Consulta enviada correctamente',
-		});
-		$('#sendMailForm').trigger("reset");
-		$.ajax({
-		  type: 'POST',
-		  url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-		  data: {
-		    'key': 'NzFIugG_1HSaBK8S2HPQwQ',
-		    'message': {
-		      'from_email': 'emiliano.rot@ejer.com.ar',
-		      'to': [
-		          {
-		            'email': 'emiliano.rot@ejer.com.ar',
-		            'type': 'to'
-		          },
-		        ],
-		      'autotext': 'true',
-		      'subject': 'Consulta recibida - EJER.COM.AR',
-		      'html': 'Nombre: ' + $("#name").val() + '<br> Telefono: ' + $("#contact").val() + '<br> Mail: ' + $("#mail").val() + '<br><br> Consulta: ' + $("#message").val(),
-		    }
-		  }
-		 }).done(function(response) {
-		   console.log(response); 
-		 	});
-	});	 
+  $("#btn-contactox").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: contacto,
+      },
+      500
+    );
+  });
 
-	
+  // ------ funcionamiento del formulario
 
-	$("#sendMailForm2").submit(function(e) {
-		e.preventDefault();
-		Swal.fire({
-		  icon: 'success',
-		  title: 'Éxito',
-		  text: 'Consulta enviada correctamente',
-		});
-		$('#sendMailForm2').trigger("reset");
-		$.ajax({
-		  type: 'POST',
-		  url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-		  data: {
-		    'key': 'NzFIugG_1HSaBK8S2HPQwQ',
-		    'message': {
-		      'from_email': 'emiliano.rot@ejer.com.ar',
-		      'to': [
-		          {
-		            'email': 'emiliano.rot@ejer.com.ar',
-		            'type': 'to'
-		          },
-		        ],
-		      'autotext': 'true',
-		      'subject': 'Consulta recibida - EJER.COM.AR',
-		      'html': 'Nombre: ' + $("#name2").val() + '<br> Telefono: ' + $("#contact2").val() + '<br> Mail: ' + $("#mail2").val() + '<br><br> Consulta: ' + $("#message2").val(),
-		    }
-		  }
-		 }).done(function(response) {
-		   console.log(response); 
-		 	});
-	}); 
+  const name = document.getElementById("name");
+  const name2 = document.getElementById("name2");
+  const phone = document.getElementById("phone");
+  const phone2 = document.getElementById("phone2");
+  const email = document.getElementById("email");
+  const email2 = document.getElementById("email2");
+  const message = document.getElementById("message");
+  const message2 = document.getElementById("message2");
 
+  document
+    .getElementById("sendMailForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const serviceID = "default_service";
+      const templateID = "template_84wdq6p";
+
+      emailjs.sendForm(serviceID, templateID, this).then(
+        () => {
+          Swal.fire({
+            icon: "success",
+            title: "Éxito",
+            text: "Consulta enviada correctamente",
+          });
+          name.value = "";
+          phone.value = "";
+          email.value = "";
+          message.value = "";
+        },
+        (err) => {
+          alert(JSON.stringify(err));
+        }
+      );
+    });
+
+  document
+    .getElementById("sendMailForm2")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const serviceID = "default_service";
+      const templateID = "template_84wdq6p";
+
+      emailjs.sendForm(serviceID, templateID, this).then(
+        () => {
+          Swal.fire({
+            icon: "success",
+            title: "Éxito",
+            text: "Consulta enviada correctamente",
+          });
+          name2.value = "";
+          phone2.value = "";
+          email2.value = "";
+          message2.value = "";
+        },
+        (err) => {
+          alert(JSON.stringify(err));
+        }
+      );
+    });
 });
